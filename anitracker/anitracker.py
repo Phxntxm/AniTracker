5be089e435e1bc849fcac4219427e93e1220baf6
Dan Hess <phxntxm1@gmail.com>
2021-07-29 15:49:26 -0800

    Split player into its own file

--- anitracker/anitracker.py
+++ anitracker/anitracker.py
@@ -30,6 +30,7 @@ from anitracker.config import Config
 from anitracker.media import AnimeCollection, AnimeFile, UserStatus
 from anitracker.media.anime import NyaaResult, SubtitleTrack
 from anitracker.sync import AniList
+from anitracker.player import Player
 
 if TYPE_CHECKING:
     from anitracker.__main__ import MainWindow
@@ -93,7 +94,7 @@ class AniTracker:
 
         self._anilist = AniList()
         self._episodes: Dict[Tuple[str, int], AnimeFile] = {}
-        self._standalone_subtitles: Dict[Tuple[str, int], str] = {}
+        self.standalone_subtitles: Dict[Tuple[str, int], str] = {}
         self._anilist.from_config(self._config)
 
     @property
@@ -192,25 +193,22 @@ class AniTracker:
         if episode is None:
             raise TypeError(f"Could not find episode {episode_num} for {anime}")
 
-        # Start with just this episode
-        episode.load_subtitles(self._standalone_subtitles)
-        self._play_episodes([(episode, self._get_sub_for_episode(episode))], anime, window)
+        player = Player(anime, [episode], self, window)
+        player.start()
 
     def start_playlist(
         self, anime: AnimeCollection, starting_episode: int, window: MainWindow
     ):
         """Starts a playlist for an anime from this episode on"""
-        episodes: List[Tuple[AnimeFile, Optional[SubtitleTrack]]] = []
+        episodes = [
+            ep
+            for ep in self.get_episodes(anime)
+            if ep.episode_number >= starting_episode
+        ]
+        episodes.sort(key=lambda e: e.episode_number)
 
-        for index in range(starting_episode, anime.episode_count + 1):
-            episode = self.get_episode(anime, index)
-
-            if episode:
-                episode.load_subtitles(self._standalone_subtitles)
-                episodes.append((episode, self._get_sub_for_episode(episode)))
-
-        if episodes:
-            self._play_episodes(episodes, anime, window)
+        player = Player(anime, episodes, self, window)
+        player.start()
 
     def _anime_is_title(
         self, anime: AnimeCollection, title: str, *, ratio: int = 80
@@ -222,191 +220,6 @@ class AniTracker:
                 return True
 
         return False
-
-    def _get_sub_for_episode(self, episode: AnimeFile) -> Optional[SubtitleTrack]:
-        # If there's no subtitles, return None
-        if not episode.subtitles:
-            return None
-        # Create copy so that we can pop safely
-        subs = episode.subtitles.copy()
-        # Set up a priority system, starting with least to most priority
-        # Set the very first episode as the first priority
-        priority = subs.pop(0)
-
-        # Check if we want to skip songs_signs, and we have a songs/signs one
-        # if we do, we want to skip it as priority if there is a non-songs/signs
-        if self._config["skip_songs_signs"] and priority.is_songs_signs:
-            # Loop through all the leftover subs
-            for sub_track in subs:
-                # If we found one that's not signs/subs then set that as priority
-                if not sub_track.is_songs_signs:
-                    priority = sub_track
-                    subs.pop(subs.index(priority))
-                    break
-
-        # If this is also the language we want, then we don't need to
-        # do any more searching
-        if self._config["subtitle"] != priority.language:
-            # Means our priority set isn't the language we want, so try to find one
-            for sub_track in subs:
-                if sub_track.language == self._config["subtitle"]:
-                    priority = sub_track
-                    break
-
-        # Now return whatever our highest priority was
-        logger.info(f"Using subtitle track {priority} for episode {episode}")
-        return priority
-
-    def _get_mpv_command(
-        self, episodes: List[Tuple[AnimeFile, Optional[SubtitleTrack]]], anime: AnimeCollection
-    ) -> List[str]:
-        # Setup the command
-        cmd = []
-        # ATM this doesn't support more than just windows or linux
-        if not sys.platform.startswith(("linux", "win32")):
-            raise TypeError("Unsupported platform")
-
-        # Add the mpv command
-        if frozen_path is not None:
-            cmd.extend(shlex.split(f"{frozen_path}/mpv", posix=not sys.platform.startswith("win32")))  # type: ignore
-        else:
-            cmd.append("mpv")
-
-        # Add the language priority
-        cmd.append("--alang=jpn,en")
-
-        # Smooths things out for larger playlists
-        cmd.extend(["--profile=sw-fast", "--hwdec=auto"])
-
-        # Add the status message
-        cmd.extend(
-            ["--term-status-msg='Perc: ::${percent-pos}:: Pos: ::${playback-time}::'"]
-        )
-
-        # Add the subtitle and file pairs
-        for ep, sub in episodes:
-            cmd.append(r"--{")
-            cmd.append(ep.file)
-            # Append subtitle track
-            if sub:
-                if sub.file:
-                    cmd.append(f"--sub-file={sub.file}")
-                else:
-                    cmd.append(f"--sid={sub.id}")
-            # Append progress if we can find it
-            pos = self._config.get_option(
-                f"{anime.id}-{ep.episode_number}", section="EpisodeProgress"
-            )
-            if pos is not None:
-                cmd.append(f"--start={pos}")
-            cmd.append(r"--}")
-
-        return cmd
-
-    def _play_episodes(
-        self,
-        episodes: List[Tuple[AnimeFile, Optional[SubtitleTrack]]],
-        anime: AnimeCollection,
-        window: MainWindow,
-    ):
-        cmd = self._get_mpv_command(episodes, anime)
-        logger.info(f"Running mpv command: {cmd}")
-
-        proc = subprocess.Popen(
-            cmd,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            stdin=subprocess.DEVNULL,
-        )
-
-        perc: int = 0
-        last_updated: float = time.monotonic()
-        # Start at the first episode in the playlist of course
-        current_ep = episodes[0][0]
-
-        # Parses the output of the MPV process to determine episode incrementing
-        while proc.poll() is None:
-            if proc.stdout is not None:
-                stdout = proc.stdout.readline().decode().strip()
-                # Skip if blank
-                if not stdout:
-                    continue
-                if match := re.findall(
-                    r"Perc: ::(\d+):: Pos: ::(\d{2}:\d{2}:\d{2})::", stdout
-                ):
-                    # The way this works is strange because of the escaping used
-                    # to overwrite console. User friendly for viewing, not auto
-                    # parsing friendly. In this same stdout line there will be every
-                    # percentage update found during the watch, we want the latest (last)
-                    perc_str, pos = match[-1]  # type: ignore
-                    perc = int(perc_str)
-                    if perc > 80:
-                        now = time.monotonic()
-                        # To prevent race conditions, lets ensure we don't try to update
-                        # more than once in the span of 2 seconds. I've seen weird cases
-                        # where the output sends the percentage for the previous episode
-                        # at the exact time that it switches episodes. The order of my
-                        # checking of things should prevent that causing an issue, but I've
-                        # twice in hundreds of tests where the order seemed wrong. Realistically,
-                        # sounds like a probable bug in MPV but unrealistic to get fixed as it
-                        # will be low priority due to how niche it is
-                        if now - last_updated > 2:
-                            logger.info(
-                                f"Updating episode {current_ep}, progress was {perc}%"
-                            )
-                            self._increment_episode(current_ep, window)
-                        self._remove_position_for_episode(current_ep, anime, pos)
-                        last_updated = now
-                    else:
-                        self._save_position_for_episode(current_ep, anime, pos)
-                elif stdout.startswith("Playing: "):
-                    # Now strip off the playing to get the filename
-                    stdout = stdout.split("Playing: ")[1]
-                    # Now find the file that matches the name
-                    current_ep = next(f[0] for f in episodes if f[0].file == stdout)
-
-            time.sleep(0.05)
-
-    def _increment_episode(self, episode: AnimeFile, window: MainWindow):
-        coll = self.get_anime(episode.title)
-        if coll is None:
-            return
-
-        # First skip this if it's not the next episode
-        if coll.progress != episode.episode_number - 1:
-            return
-
-        # The update variables that will be sent
-        vars: Dict[str, Any] = {"progress": episode.episode_number}
-
-        # Include completion if this is the last episode
-        if episode.episode_number == coll.episode_count:
-            if coll.user_status == UserStatus.REPEATING:
-                vars["status"] = UserStatus.COMPLETED
-                vars["repeat"] = coll.repeat + 1
-            else:
-                vars["status"] = UserStatus.COMPLETED
-                vars["completed_at"] = datetime.now().date()
-        # If this was the first episode, include started at
-        if coll.progress == 0 and coll.user_status is not UserStatus.REPEATING:
-            vars["started_at"] = datetime.now().date()
-
-        coll.edit(self._anilist, **vars)
-
-        # Now trigger an update of the app
-        window.anime_updater.start()
-
-    def _save_position_for_episode(self, episode: AnimeFile, anime: AnimeCollection, position: str):
-        self._config.set_option(
-            f"{anime.id}-{episode.episode_number}",
-            position,
-            section="EpisodeProgress",
-        )
-
-    def _remove_position_for_episode(self, episode: AnimeFile, anime: AnimeCollection, position: str):
-        self._config.remove_option(
-            f"{anime.id}-{episode.episode_number}", section="EpisodeProgress"
-        )
 
     def _refresh_anime_folder(self):
         try:
@@ -444,7 +257,7 @@ class AniTracker:
                     yield result
             # Otherwise if it's a subtitle track, store it
             if data.get("extension", "").lower() in subtitle_file_extensions:
-                self._standalone_subtitles[
+                self.standalone_subtitles[
                     (data["anime_title"], int(data["episode"]))
                 ] = str(file)
 
