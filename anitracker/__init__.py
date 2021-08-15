a5b06fb3d291abbf833d237eae0b93720586addd
Dan Hess <phxntxm1@gmail.com>
2021-08-15 08:51:33 -0800

    Version bump

--- anitracker/__init__.py
+++ anitracker/__init__.py
@@ -1,7 +1,7 @@
 import sys
 import typing
 
-__version__ = "1.3.6"
+__version__ = "1.3.7"
 user_agent = f"AniTracker/{__version__} (Language=py)"
 frozen_path: typing.Optional[str] = getattr(sys, "_MEIPASS", None)
 
